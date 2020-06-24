79f3a8f0c800bca742a3ab3e41dfe253635522d8
Bob Tate <btate712@gmail.com>
2020-06-24 02:32:02 -0500

    tiles layed out and displayed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
+const WIDTH = 4;
+
+const board = document.getElementById("board");
+
 function createGame() {
-  const board = document.querySelector(".board");
-  console.log(board);
+  for(let i = 0; i < WIDTH * WIDTH; i++) {
+    newTile = document.createElement("div");
+    newTile.innerHTML = "0";
+    newTile.className = "tile";
+    newTile.id = `tile${i}`;
+    board.appendChild(newTile);
+  }
 }
+
+createGame();
+console.log(board);
